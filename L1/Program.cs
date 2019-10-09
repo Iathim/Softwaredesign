eca6b5176bf67d2293809ae3b680ffeb8947d467
Iathim <45171208+Iathim@users.noreply.github.com>
2019-10-09 16:25:43 +0200

    main method changed

--- L1/Program.cs
+++ L1/Program.cs
@@ -6,15 +6,15 @@ namespace L1
     {
         static void Main(string[] args)
         {
-            /*Console.WriteLine("Bitte einen kleinen Satz eingeben");
+            Console.WriteLine("Bitte einen kleinen Satz eingeben");
             Console.Write("> ");
             var text = Console.ReadLine();
             string letters = Functions.reverseLetters(text); 
             string words = Functions.reverseWords(text);
             string sentence = Functions.reverseSentence(text);
-            Console.WriteLine(sentence + "\n" + words + "\n" + letters);*/ 
+            Console.WriteLine(sentence + "\n" + words + "\n" + letters);
 
-            string word = "Satz zum Testen"; 
+            /*string word = "Satz zum Testen"; 
 
             //word = Functions.reverseLetters(word);
             
@@ -22,7 +22,7 @@ namespace L1
 
             word = Functions.reverseSentence(word); 
 
-            Console.WriteLine(word); 
+            Console.WriteLine(word); */
 
 
         }
