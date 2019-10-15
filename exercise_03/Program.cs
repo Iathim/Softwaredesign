596b41a0cd596ef25281c00805fa48f6e2867684
Iathim <45171208+Iathim@users.noreply.github.com>
2019-10-15 10:04:14 +0200

    exercise 03 added

---
+++ exercise_03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+public class SimplePerson
+{
+   public string FirstName;
+   public string LastName;
+   public DateTime DateOfBirth;
+}
+
+namespace Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person root = Familytree.BuildTree();
+
+            Person found = Familytree.Find(root);
+            //Debugger: Dad: Philip Battenberg, Mum: Alice Battenberg; Charlie Wales; Dad: Schorsch-Albert York, Mum: Elese Windsor; Lisbeth Bowes-Lyon; Willi Cambridge
+
+            WriteLine(found);
+        }
+    }
+}
---
+++ exercise_03/ToDo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exercise_03
+{
+    class ToDo
+    {
+        public class Person
+        {
+            public string FirstName;
+            public string LastName;
+            public DateTime Age;
+
+            /*public Person (string FirstName, string LastName, DateTime Age)
+            { 
+                this.FirstName =  FirstName; 
+                this.LastName = LastName; 
+                this.Age = Age; 
+            }*/
+
+            public override string ToString () 
+            { 
+               
+
+
+                return ""; 
+            }
+
+             
+
+        }
+    }
+}
