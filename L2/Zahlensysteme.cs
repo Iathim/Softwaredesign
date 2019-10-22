1cfd2e017ed38f6d1739003197539533bccd78bb
Iathim <45171208+Iathim@users.noreply.github.com>
2019-10-22 12:53:58 +0200

    L2 and exercise 6 started

---
+++ L2/Zahlensysteme.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace L2
+{
+    class Zahlensysteme
+    {
+       public static int ConvertDecimalToHexal(int dec)
+       {
+           if (dec > 1023 || dec < 0)
+           {
+               Console.WriteLine("Die Zahl darf nicht größer als 1023 und kleiner als 0 sein"); 
+               return 0; 
+           }
+
+           if(dec >= 6)
+                return dec; 
+
+           else 
+           {
+              int mod = dec % 6; 
+              int next = dec / 6; 
+
+              if (next != 0)
+              ConvertDecimalToHexal(next); 
+              
+               return 0; 
+           }
+       }
+    }
+}
---
+++ exercise_06/PseudoProgram.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace exercise_06
+{
+    class PseudoProgram
+    {
+        int geschirr; 
+
+        Boolean wasserMitSpühlmittel = false; 
+
+        int wasserstand = 0; 
+
+        public static int geschirrSammeln(int geschirr)
+        {
+            int geschirrSammlung = geschirr; 
+
+            return geschirrSammlung; 
+        }
+
+
+        public static Boolean entscheidung(Boolean motivationSpühlen)
+        {
+            if (motivationSpühlen == true)
+            {
+                return true; 
+            }
+            
+            else 
+            {
+                return false; 
+            }
+        }
+
+
+        public static int wasserEinlassen() 
+        {
+            int wasserstand = 0; 
+
+            int beckenVoll = 3; 
+
+            int wasserProMinute = 1; 
+
+            for (int i = 0; wasserstand != beckenVoll; i++ )
+            {
+                wasserstand = wasserstand + wasserProMinute; 
+            }
+
+            return wasserstand; 
+        }
+
+
+        public static Boolean spühlmittelHinzugeben(Boolean wasserMitSpühlmittel)
+        {
+            wasserMitSpühlmittel = true; 
+
+            return wasserMitSpühlmittel; 
+        }
+
+
+        public static Boolean geschirrInSpühle(Boolean geschirrInWasser)
+        {
+            geschirrInWasser = true; 
+
+            return geschirrInWasser; 
+        }
+
+
+        public static Boolean geschirrSpühlen(int geschirrSammlung)
+        {   
+            Boolean geschirrGespühlt = false; 
+
+            for (int i = 0; i < geschirrSammlung; i++)
+            {
+                
+            }
+            
+
+            return geschirrGespühlt; 
+
+        }
+
+    }
+}
