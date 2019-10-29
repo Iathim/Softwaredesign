f9c976894b0bb2fed7bc11f05c229e167c23451b
Iathim <45171208+Iathim@users.noreply.github.com>
2019-10-29 10:51:47 +0100

    XOX program started

---
+++ L3/XOX.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace L3
+{
+    class XOX
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public static Boolean startGame()
+        {
+            Boolean gameEnd = false; 
+
+            Console.WriteLine("Bitte X oder O eingeben"); 
+
+            var symbol = Console.ReadLine();
+
+            string[,] arraySpielfeld = new string[3, 3] { { " ", " ", " " }, { " ", " ", " "}, { " ", " ", " "} };
+
+            if (symbol != "X" || symbol != "O" || symbol != "x" || symbol != "o" )
+            { 
+                Console.WriteLine("Bitte gültiges Symbol eingeben. Nur X oder O"); 
+            }
+
+
+
+
+
+
+
+
+
+            
+
+
+
+            return gameEnd; 
+        }
+    }
+}
