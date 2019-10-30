49a1c3f1c84e3f4a7709504d20c19b3b82a4b7e6
Iathim <45171208+Iathim@users.noreply.github.com>
2019-10-30 11:14:35 +0100

    XOX.cs updated

--- L3/XOX.cs
+++ L3/XOX.cs
@@ -10,19 +10,60 @@ namespace L3
         }
 
 
-        public static Boolean startGame()
+        public static string startGame()
         {
             Boolean gameEnd = false; 
+            int turn = 0; 
 
-            Console.WriteLine("Bitte X oder O eingeben"); 
+            int spieler = 2; 
+            
 
-            var symbol = Console.ReadLine();
 
-            string[,] arraySpielfeld = new string[3, 3] { { " ", " ", " " }, { " ", " ", " "}, { " ", " ", " "} };
 
-            if (symbol != "X" || symbol != "O" || symbol != "x" || symbol != "o" )
-            { 
-                Console.WriteLine("Bitte gültiges Symbol eingeben. Nur X oder O"); 
+            Console.WriteLine("Spieler 1: Bitte X oder O eingeben"); 
+
+            string[,] arrayGameBoard = new string[3, 3] { { "0", "1", "2" }, { "3", "4", "5"}, { "6", "7", "8"} };
+
+            Console.WriteLine(arrayGameBoard); 
+
+
+            for (int i = 0; turn <= 9; i++)
+            {
+                var symbol = Console.ReadLine();
+
+                if (symbol != "X" || symbol != "O")
+                { 
+                    Console.WriteLine("Bitte gültiges Symbol eingeben. Nur X oder O in upper case"); 
+                }
+
+            
+                Console.WriteLine("Bitte geben Sie die Zahl des Feldes an, in welches Sie ihr Symbol setzen wollen."); 
+                
+                string position = Console.ReadLine(); 
+
+                /*if (spieler == 2)
+                    spieler = 1;
+                else if (spieler == 1)
+                    spieler = 2;*/
+
+                switch(position)
+                {
+                    case "0": arrayGameBoard[0,0] = symbol; 
+                    break; 
+
+                    case "1": arrayGameBoard[0,1] = symbol; 
+                    break;
+
+                    case "2": arrayGameBoard[0,2] = symbol; 
+                    break; 
+
+
+                }
+
+
+
+                turn = turn + 1; 
+
             }
 
 
@@ -31,24 +72,41 @@ namespace L3
 
 
 
+            
 
 
-            if (arraySpielfeld[1,1] == "X")
+           
+
+
+
+            return "Es gibt einen Gewinner!"; 
+        }
+
+
+        public static Boolean winningCondition(string symbol)
+        {
+            Boolean win = false; 
+
+            /*if (arrayGameBoard[0,0] == symbol)
             {
-                if (arraySpielfeld[2,2] == "X")
+                if (arrayGameBoard[1,1] == symbol)
                 {
-                    if (arraySpielfeld[3,3] == "X")
+                    if (arrayGameBoard[2,2] == symbol)
                     {
-                        gameEnd = true; 
+                        win = true;
 
-                        Console.WriteLine("Herzlichen Glückwunsch, X hat gewonnen!"); 
+                        return win; 
                     }
                 }
-            }
+            }*/
 
 
 
-            return gameEnd; 
+
+
+
+
+            return win; 
         }
     }
 }
