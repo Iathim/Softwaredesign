f6704de2b6dbc55237a6ba4c2b3e47cc526bb862
Iathim <45171208+Iathim@users.noreply.github.com>
2019-11-26 10:32:07 +0100

    displayMenue added

---
+++ _Quiz(new)/Menue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _Quiz_new_
+{
+    class Menue 
+    {
+        public static string displayMenue()
+        {
+            Console.WriteLine("1. Play Quiz."); 
+            Console.WriteLine("2. Create a new Question & Answer."); 
+            Console.WriteLine("3. Exit."); 
+
+            Console.WriteLine("Please enter the number of the selection you want to chose."); 
+
+            string userInput = Console.ReadLine(); 
+
+            if (userInput != "1" || userInput != "2" || userInput != "3")
+            {
+                Console.WriteLine("Please enter a number from 1 to 3."); 
+                //displayMenue(); 
+                return null; 
+            }
+
+            else 
+            {
+                return userInput; 
+            }
+        }
+    }
+}
--- _Quiz(new)/Quiz.cs
+++ _Quiz(new)/Quiz.cs
@@ -2,8 +2,44 @@
 
 namespace _Quiz_new_
 {
-    class Main
+    class MainQuiz
     {
-        
+        public static void Main(string[] args)
+        {
+            
+            
+            
+            
+            
+            
+            
+            string userInput = Menue.displayMenue(); 
+
+            if (userInput == null)
+            {
+                userInput = Menue.displayMenue(); 
+            }
+
+            else
+            {
+                if (userInput == "1")
+                {
+                    //Play Quiz 
+                }
+
+                if (userInput == "2")
+                {
+                    //Create new Quizelement 
+                    //Returns Quizelement which has to be saved to the QuizelementList/Question
+                }
+
+                if (userInput == "3")
+                {
+                    //exit game 
+                }
+            }
+
+
+        }
     }
 }
