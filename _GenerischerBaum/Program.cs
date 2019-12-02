bacd9b9e7520486cfabba300689ac330e38d4f0d
Iathim <45171208+Iathim@users.noreply.github.com>
2019-12-02 18:42:18 +0100

    methods implemented

--- _GenerischerBaum/Program.cs
+++ _GenerischerBaum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _GenerischerBaum
 {
@@ -6,7 +7,23 @@ namespace _GenerischerBaum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tree = new TreeNode<String>();
+            var root = tree.CreateNode("root");
+            var child1 = tree.CreateNode("child1");
+            var child2 = tree.CreateNode("child2");
+            root.AppendChild(child1);
+            root.AppendChild(child2);
+            var grand11 = tree.CreateNode("grand11");
+            var grand12 = tree.CreateNode("grand12");
+            var grand13 = tree.CreateNode("grand13");
+            child1.AppendChild(grand11);
+            child1.AppendChild(grand12);
+            child1.AppendChild(grand13);
+            var grand21 = tree.CreateNode("grand21");
+            child2.AppendChild(grand21);
+            child1.RemoveChild(grand12);
+
+            root.PrintTree(); 
         }
     }
 }
---
+++ _GenerischerBaum/Tree.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _GenerischerBaum
+{
+    public class TreeNode<T>
+    {
+        public T TreeContent;
+        public List<TreeNode<T>> Children;
+        
+        public TreeNode()
+        {
+            this.Children = new List<TreeNode<T>>();
+        }
+
+
+        public TreeNode(T contents)
+        {
+            this.Children = new List<TreeNode<T>>();
+            
+            this.TreeContent = contents;
+        }
+
+
+
+        public TreeNode<T> CreateNode(T newObject)
+        {
+            return new TreeNode<T>(newObject);
+        }
+
+
+        public void AppendChild(TreeNode<T> child)
+        {
+            TreeNode<T> newChildNode = child;
+            
+            Children.Add(newChildNode);
+        }
+
+
+        public void RemoveChild(TreeNode<T> childNode)
+        {
+            for (int i = 0; i < Children.Count; i++)
+            {
+                if (Children[i].Equals(childNode))
+                {
+                    Children.RemoveAt(i);
+                }
+            }
+        }
+
+
+        private void PrintChildren(TreeNode<T> parent, string generationIndex)
+        {
+            generationIndex = generationIndex + "*";
+
+            foreach(TreeNode<T> child in parent.Children)
+            {
+                Console.WriteLine(generationIndex + child.TreeContent.ToString());
+
+                PrintChildren(child, generationIndex);
+            }
+        }
+
+        
+        public void PrintTree()
+        {
+            Console.WriteLine(this.TreeContent.ToString());
+
+            PrintChildren(this, "*");
+        }
+
+
+        public void ClearTree()
+        {
+            this.Children.Clear();
+        }
+    }
+}
