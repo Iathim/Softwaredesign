34bae0733652212a5e16030cf40a76c7c16cf986
Iathim <45171208+Iathim@users.noreply.github.com>
2019-12-16 22:44:37 +0100

    tried Iterator exercise

---
+++ _GenerischerBaum/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _GenerischerBaum
+{
+    public class Test
+    {
+        public int curIndex = -1; 
+        public static string[] testString = {"test1", "test2", "test3", "test4", "test5"}; 
+    }
+} 
