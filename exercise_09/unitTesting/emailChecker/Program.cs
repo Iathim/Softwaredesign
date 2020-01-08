c2199aa51556d65c808ff2b9f7d6e6b08b945561
Iathim <45171208+Iathim@users.noreply.github.com>
2020-01-08 10:38:54 +0100

    Update Program.cs

--- exercise_09/unitTesting/emailChecker/Program.cs
+++ exercise_09/unitTesting/emailChecker/Program.cs
@@ -11,33 +11,10 @@ namespace exercise_09
             string mail3 = "m@."; //true 
             string mail4 ="7@gmail.com"; //true
 
-            Console.WriteLine(mail.IsEmailAddress(mail1)); 
-            if (mail.IsEmailAddress(mail1))
-                Console.WriteLine("TEST PASSED: " + mail1 + " korrekt als Email-Adresse erkannt");
-            else
-                Console.WriteLine("TEST FAILED: " + mail1 + " nicht als Email-Adresse erkannt, obwohl korrekt.");
-
-
-            Console.WriteLine(mail.IsEmailAddress(mail2)); 
-            if (mail.IsEmailAddress(mail2))
-                Console.WriteLine("TEST PASSED: " + mail2 + " korrekt als Email-Adresse erkannt");
-            else
-                Console.WriteLine("TEST FAILED: " + mail2 + " nicht als Email-Adresse erkannt, obwohl korrekt.");
-
-
-            Console.WriteLine(mail.IsEmailAddress(mail3)); 
-            if (mail.IsEmailAddress(mail3))
-                Console.WriteLine("TEST PASSED: " + mail3 + " korrekt als Email-Adresse erkannt");
-            else
-                Console.WriteLine("TEST FAILED: " + mail3 + " nicht als Email-Adresse erkannt, obwohl korrekt.");
-
-
-
-            Console.WriteLine(mail.IsEmailAddress(mail4)); 
-            if (mail.IsEmailAddress(mail4))
-                Console.WriteLine("TEST PASSED: " + mail4 + " korrekt als Email-Adresse erkannt");
-            else
-                Console.WriteLine("TEST FAILED: " + mail4 + " nicht als Email-Adresse erkannt, obwohl korrekt.");
+            mail.Test(mail1); 
+            mail.Test(mail2); 
+            mail.Test(mail3); 
+            mail.Test(mail4); 
         }
     }
 
@@ -50,5 +27,14 @@ namespace exercise_09
                 int iDot = emailAddress.LastIndexOf('.');
                 return (iAt > 0 && iDot > iAt);
             }
+
+
+        public static string Test (string email)
+        {
+            if (mail.IsEmailAddress(email))
+                return("TEST PASSED: " + email + " korrekt als Email-Adresse erkannt");
+            else
+                return("TEST FAILED: " + email + " nicht als Email-Adresse erkannt.");
+        }
     }
 }
