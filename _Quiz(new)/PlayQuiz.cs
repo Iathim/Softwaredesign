8b38f5b5aa990fce7d3554174c54a7090c407a93
Iathim <45171208+Iathim@users.noreply.github.com>
2020-01-08 10:02:20 +0100

    Update PlayQuiz.cs

--- _Quiz(new)/PlayQuiz.cs
+++ _Quiz(new)/PlayQuiz.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO; 
+using System.Text.Json; 
 
 namespace _Quiz_new_
 {
@@ -9,7 +10,10 @@ namespace _Quiz_new_
         {
             StreamReader file = new StreamReader(@"Quizelements.json");
 
-            //Json.net 
+            //Json.net https://www.newtonsoft.com/json
+
+            
+            //Quizelemente in Liste speichern, neu erstellte Elemente hinzufügen, JSON löschen, JSON neu mit Liste erstellen 
         }
     }
 }
