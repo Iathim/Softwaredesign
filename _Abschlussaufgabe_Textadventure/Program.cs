55e59b1caa98710de7c57809f62fe2c4432e39a0
Iathim <45171208+Iathim@users.noreply.github.com>
2020-01-14 00:04:44 +0100

    created standard files

---
+++ _Abschlussaufgabe_Textadventure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Abschlussaufgabe_Textadventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
