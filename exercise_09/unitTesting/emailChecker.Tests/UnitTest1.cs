cbbc263fbd722230397f31ba2680e9c3714b3501
Iathim <45171208+Iathim@users.noreply.github.com>
2020-01-14 00:09:55 +0100

    created Test

--- exercise_09/unitTesting/emailChecker.Tests/UnitTest1.cs
+++ exercise_09/unitTesting/emailChecker.Tests/UnitTest1.cs
@@ -8,7 +8,9 @@ namespace emailChecker.Tests
         [Fact]
         public void Test1()
         {
-
+            string mailaddress = "ich@provider.com";
+            bool result = EmailChecker.Program.IsEmailAddress(mailaddress);
+            Assert.True(result, mailaddress + " nicht als Email-Adresse erkannt, obwohl korrekt.");
         }
     }
 }
