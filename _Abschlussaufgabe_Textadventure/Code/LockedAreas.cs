ed9ecc8a812b1fe5b46f817f8b075181158e0800
Iathim <45171208+Iathim@users.noreply.github.com>
2020-02-08 18:47:46 +0100

    attribute added, method changed

--- _Abschlussaufgabe_Textadventure/Code/LockedAreas.cs
+++ _Abschlussaufgabe_Textadventure/Code/LockedAreas.cs
@@ -7,16 +7,18 @@ namespace Code
     {
         public bool isLocked = true;
         public string reasonWhyLocked;
-        public string consequenz;     
+        public string consequenzFailure;  
+        public string consequenzSuccess;    
         public string riddle;
         public string riddleAnswer; 
 
 
         public static void riddleSolution(LockedArea area, Character character)
         {
-            Console.WriteLine("You tried to open the door but failed miserably."); 
+            /*Console.WriteLine("You tried to open the door but failed miserably."); 
             Console.WriteLine("Next to the door a panel started blinking after extending out of the wall."); 
-            Console.WriteLine("You go closer to the panel and realise there is a riddle written."); 
+            Console.WriteLine("You go closer to the panel and realise there is a riddle written.");*/ 
+            Console.WriteLine(area.reasonWhyLocked); 
 
             Console.WriteLine(area.riddle); 
             Console.WriteLine("What is your answer to the riddle? Please type it in."); 
@@ -26,16 +28,20 @@ namespace Code
 
             if (userInput == area.riddleAnswer)
             {
-                Console.WriteLine("The console started blinking before a light you didn't see when you first looked at it turned green."); 
-                Console.WriteLine("It seems you managed to unlock the door. Well done!"); 
+                /*Console.WriteLine("The console started blinking before a light you didn't see when you first looked at it turned green."); 
+                Console.WriteLine("It seems you managed to unlock the door. Well done!"); */
+
+                Console.WriteLine(area.consequenzSuccess); 
                 
                 area.isLocked = false; 
             }
 
             else 
             {
-                Console.WriteLine("You heard a screeching sound and the panel retired back into the wall."); 
-                Console.WriteLine("Alarmed, you look around and see a small blaster pointing at you."); 
+                /*Console.WriteLine("You heard a screeching sound and the panel retired back into the wall."); 
+                Console.WriteLine("Alarmed, you look around and see a small blaster pointing at you.");*/
+
+                Console.WriteLine("The answer was wrong..."); 
 
                 Random random = new Random();
                 int randomNumber = random.Next(0, 100);
@@ -43,7 +49,9 @@ namespace Code
 
                 if (randomNumber % 2 != 0)
                 {
-                    Console.WriteLine("The gun is shooting at you and you don't duck down immediately. A shot grazes you.");
+                    //Console.WriteLine("The gun is shooting at you and you don't duck down immediately. A shot grazes you.");
+
+                    Console.WriteLine(area.consequenzFailure); 
 
                     int gunDemage = 20;  
 
@@ -64,9 +72,11 @@ namespace Code
 
                 else 
                 {
-                    Console.WriteLine("The gun doesn't seem to funktion correctly because it shoots in the wrong direction."); 
+                    /*Console.WriteLine("The gun doesn't seem to funktion correctly because it shoots in the wrong direction."); 
                     Console.WriteLine("Well you got lucky but maybe think about the answer to the riddle more."); 
-                    Console.WriteLine("You don't know if the gun fires the next time correctly"); 
+                    Console.WriteLine("You don't know if the gun fires the next time correctly");*/ 
+
+                    Console.WriteLine("You managed to dodge. Lucky you.");
                 }
             }
         }
