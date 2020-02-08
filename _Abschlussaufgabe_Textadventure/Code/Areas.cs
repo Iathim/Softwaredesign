7ab4a9b09108edba025ad801b9b665ca354219e0
Iathim <45171208+Iathim@users.noreply.github.com>
2020-02-09 00:57:29 +0100

    attribute added

--- _Abschlussaufgabe_Textadventure/Code/Areas.cs
+++ _Abschlussaufgabe_Textadventure/Code/Areas.cs
@@ -9,15 +9,17 @@ namespace Code
         public List<Item> items;
         public NPC NPC; 
         public string type; 
-        public string position;  
+        public string position;
+        public bool actualArea;   
 
-        public Area (string _description, List<Item> _items, NPC _NPC, string _type, string _position)
+        public Area (string _description, List<Item> _items, NPC _NPC, string _type, string _position, bool _actualArea)
         {
             this.description = _description; 
             this.items = _items; 
             this.NPC = _NPC; 
             this.type = _type;
             this.position = _position;
+            this.actualArea = _actualArea; 
         }
     }
 }
