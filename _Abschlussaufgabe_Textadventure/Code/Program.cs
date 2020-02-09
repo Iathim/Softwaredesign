d6d0c11accf3893132772cde3dfe596a447258ec
Iathim <45171208+Iathim@users.noreply.github.com>
2020-02-09 21:36:42 +0100

    load Json updated

--- _Abschlussaufgabe_Textadventure/Code/Program.cs
+++ _Abschlussaufgabe_Textadventure/Code/Program.cs
@@ -35,39 +35,74 @@ namespace Code
         }
         static void Main(string[] args)
         {
-            List<String[]> items = readJSON.LoadItems();
-           foreach (String[] elem in items)
+            List<Item> items = new List<Item>(); 
+            List<String[]> itemsString = readJSON.LoadItems();
+           foreach (String[] elem in itemsString)
            {
-               Console.WriteLine("Name: {0}, Description: {1}", elem[0], elem[1]);
+               items.Add(new Item(elem[0], elem[1])); 
+               //Console.WriteLine("Name: {0}, Description: {1}", elem[0], elem[1]);
            }
            Console.Write(Environment.NewLine);
 
-           List<String[]> areas = readJSON.LoadAreas();
-           foreach (String[] elem in areas)
+
+            List<Area> areas = new List<Area>(); 
+           List<String[]> areasString = readJSON.LoadAreas();
+           foreach (String[] elem in areasString)
            {
-               Console.WriteLine("Description: {0}, Items: {1}, NPC: {2}, Type: {3}, Position: {4}" ,
-                elem[0], elem[1], elem[2], elem[3], elem[4]);
+               List<Item> elem1 = new List<Item>(); 
+               NPC elem2 = null; 
+                bool elem5 = bool.Parse(elem[5]); 
+
+               areas.Add(new Area(elem[0], elem1, elem2, elem[3], elem[4], elem5)); 
+               
+               Console.WriteLine("Description: {0}, Items: {1}, NPC: {2}, Type: {3}, Position: {4}, IsActualArea: {5}" ,
+                elem[0], elem[1], elem[2], elem[3], elem[4], elem[5]);
            }
            Console.Write(Environment.NewLine);
 
-           List<String[]> npcs = readJSON.LoadNPCs();
-           foreach (String[] elem in npcs)
+
+            List<NPC> npcs = new List<NPC>(); 
+           List<String[]> npcsString = readJSON.LoadNPCs();
+           foreach (String[] elem in npcsString)
            {
+               int elem1 = int.Parse(elem[1]); 
+               int elem2 = int.Parse(elem[2]); 
+               bool elem3 = bool.Parse(elem[3]); 
+               bool elem4 = bool.Parse(elem[4]); 
+               List<Item> elem5 = new List<Item>(); 
+
+               npcs.Add(new NPC(elem[0], elem1, elem2, elem3, elem4, elem5, elem[6], elem[7])); 
+
                Console.WriteLine("Name: {0}, HP: {1}, Damage: {2}, isDead: {3}, isPlayerCharacter: {4}, Inventory: {5}, Dialogue: {6}" ,
                 elem[0], elem[1], elem[2], elem[3], elem[4], elem[5], elem[6]);
            }
            Console.Write(Environment.NewLine);
 
-           List<String[]> npcItems = readJSON.LoadNPCItems();
-           foreach (String[] elem in npcItems)
+
+            List<Item> npcItems = new List<Item>(); 
+           List<String[]> npcItemsString = readJSON.LoadNPCItems();
+           foreach (String[] elem in npcItemsString)
            {
+               npcItems.Add(new Item(elem[1], elem[2])); 
                Console.WriteLine("Name: {0}, Description: {1}", elem[0], elem[1]);
            }
 
-           List<String[]> player = readJSON.LoadPlayerCharacter();
-           foreach (String[] elem in player)
+
+            PlayerCharacter player = null; 
+           List<String[]> playerString = readJSON.LoadPlayerCharacter();
+           foreach (String[] elem in playerString)
            {
-               Console.WriteLine("Name: {0}, Description: {1}", elem[0], elem[1]);
+               int elem1 = int.Parse(elem[1]); 
+               int elem2 = int.Parse(elem[2]); 
+               bool elem3 = bool.Parse(elem[3]); 
+               bool elem4 = bool.Parse(elem[4]); 
+               List<Item> elem5 = new List<Item>(); 
+
+               player = new PlayerCharacter(elem[0], elem1, elem2, elem3, elem4, elem5); 
+               //player.Add(new PlayerCharacter(elem[0], elem1, elem2, elem3, elem4, elem5)); 
+               
+               Console.WriteLine("Name: {0}, HP: {1}, Damage: {2}, isDead: {3}, isPlayerCharacter: {4}, Inventory: {5}" ,
+                elem[0], elem[1], elem[2], elem[3], elem[4], elem[5]);
            }
         }
     }
